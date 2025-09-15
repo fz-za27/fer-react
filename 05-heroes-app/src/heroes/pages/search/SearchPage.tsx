8b7adf106073afdc5b26dbb03f262edc95421bdc
Fabián Zambrano <zambranofabian1102@gmail.com>
2025-09-14 21:58:45 -0500

    Actualización de la seccion 05-heroes-app

--- 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -1,13 +1,29 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
+import { HeroStats } from "@/heroes/components/HeroStats"
+import { SearchControls } from "./ui/SearchControls"
+import { CustomBreadcrumb } from "@/components/custom/CustomBreadcrumb"
 
 export const SearchPage = () => {
 
   return (
     <>
-      <CustomJumbotron 
-        title='Búsqueda de SuperHéroes' 
+      <CustomJumbotron
+        title='Búsqueda de SuperHéroes'
         description='Descubre, explora y administra super héroes y villanos'
       />
+
+      <CustomBreadcrumb 
+        currentPage="Buscador de héroes"
+        breadcrumbs={[
+          { label: 'home1', to: '/' },
+          { label: 'home2', to: '/' },
+          { label: 'home3', to: '/' },
+        ]}  
+      />
+
+      <HeroStats />
+
+      <SearchControls />
     </>
   )
 }
